1ab3b2b7d873bc8689580f5a0ea3355d37550fb1
Victor <fy123nig@gmail.com>
2024-10-17 12:37:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,10 +14,10 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/Bootstrap/argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/Bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' `find package -type f -path '*/default-settings/files/zzz-default-settings'`
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/Bootstrap/argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/Bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
+# sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' `find package -type f -path '*/default-settings/files/zzz-default-settings'`
 
 
 # Modify hostname
